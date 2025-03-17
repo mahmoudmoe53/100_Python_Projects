f53ef6e5de76d7d56ee180a33e7600f3fe33979d
Mahmoud <mahmoudmoe53@hotmail.com>
2025-03-17 10:23:18 +0000

    updated

--- Object_Oriented_Programming/oop-coffee-machine-start/main.py
+++ Object_Oriented_Programming/oop-coffee-machine-start/main.py
@@ -1,6 +1,7 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from CoffeeMachine.art import logo
 
 menu = Menu()
 
@@ -9,6 +10,7 @@ coffee = CoffeeMaker()
 money = MoneyMachine()
 
 while True:
+    print(logo)
     choice = input(f"what would you like? {menu.get_items()}\n").lower()
 
     if choice == "latte" or choice == "cappuccino" or choice == "espresso":
