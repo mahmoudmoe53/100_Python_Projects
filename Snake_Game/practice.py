3db2b73fa502076a68b6136d21431102a5983602
Mahmoud <mahmoudmoe53@hotmail.com>
2025-03-22 11:53:44 +0000

    Snake game almost complete

---
+++ Snake_Game/practice.py
@@ -0,0 +1,40 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Mahmoud's Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+snake.create_snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_one = True
+
+while game_is_one:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
