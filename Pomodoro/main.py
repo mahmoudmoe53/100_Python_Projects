7250db4558d84ed8029d5476abe104b354540100
Mahmoud <mahmoudmoe53@hotmail.com>
2025-04-29 10:18:11 +0100

    Pomodoro Timer completed

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -9,12 +9,40 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def timer_reset():
+    window.after_cancel(timer)
+    label.config(text="Timer", fg=GREEN)
+    tick.config(text="")
+    canvas.itemconfig(timer_text, text="00:00")
 
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 2 == 0:
+        if reps < 8:
+            count_down(short_break_sec)
+            label.config(text="Break", fg=PINK, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+        else:
+            count_down(long_break_sec)
+            label.config(text="Break", fg=RED, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+    else:
+        count_down(work_sec)
+        label.config(text="Work", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     minutes = math.floor(count / 60)
@@ -24,7 +52,21 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
+        mark = ""
+        work_sessions = math.floor(reps / 2)
+
+        for i in range(work_sessions):
+            mark += "✔"
+
+        tick.config(text=mark)
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -45,10 +87,10 @@ label.grid(column=2, row=0)
 start = Button(text="Start", bg=YELLOW, highlightthickness=0, command=start_timer)
 start.grid(column=1, row=2)
 
-reset = Button(text="Reset", bg=YELLOW, highlightthickness=0)
+reset = Button(text="Reset", bg=YELLOW, highlightthickness=0, command=timer_reset)
 reset.grid(column=3, row=2,)
 
-tick = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+tick = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
 tick.grid(column=2, row=3)
 
 
