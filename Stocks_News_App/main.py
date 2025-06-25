d9255f1ad9cdca9a1a8ae8a0da5d10ebcebcd2dc
Mahmoud <mahmoudmoe53@hotmail.com>
2025-06-25 12:41:33 +0100

    Complete

--- Stocks_News_App/main.py
+++ Stocks_News_App/main.py
@@ -71,15 +71,3 @@ for article in formatted_articles:
 
     print(message.status)
 
-
-
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
