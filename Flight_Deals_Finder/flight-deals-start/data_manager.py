d22f2ebc0f3d22f13799f52fd3283deeb6472076
Mahmoud <mahmoudmoe53@hotmail.com>
2025-07-04 09:05:18 +0100

    Unable to complete project as monthly request quota has been reached for API. Need to complete next month

---
+++ Flight_Deals_Finder/flight-deals-start/data_manager.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SHEETY_ENDPOINT = "https://api.sheety.co/f30a6a5c4535fc80adc15c5be476ce55/flightDeals/prices"
+
+class DataManager:
+    def __init__(self):
+        self.__sheety_token = os.environ.get("SHEETY_TOKEN")
+        self.__headers = {
+            "Authorization":self.__sheety_token,
+            "Content-Type": "application/json"
+        }
+        self.destination_data = {}
+
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT, headers=self.__headers)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(url=f"{SHEETY_ENDPOINT}/{city['id']}", headers=self.__headers, json=new_data)
+            print(response.text)
---
+++ Flight_Deals_Finder/flight-deals-start/flight_search.py
@@ -0,0 +1,49 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+END_POINT = "https://test.api.amadeus.com/v1"
+
+class FlightSearch:
+    def __init__(self):
+        self.__api_key = os.environ.get("AMADEUS_API_KEY")
+        self.__password = os.environ.get("AMADEUS_SECRET")
+        self.__token = self.__get_new_token()
+
+    def get_destination_code(self, city_name):
+        headers = {
+            "Authorization":f"Bearer {self.__token}"
+        }
+
+        params = {
+            "keyword":city_name,
+            "max": 2,
+            "include":"AIRPORTS"
+        }
+
+        response = requests.get(f"{END_POINT}/reference-data/locations/cities", params=params, headers=headers)
+
+        data = response.json()
+
+        print(f"{data} {city_name}")
+
+
+    def __get_new_token(self):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        body = {
+            "grant_type": 'client_credentials',
+            "client_id": self.__api_key,
+            "client_secret": self.__password
+        }
+        response = requests.post(url=f"{END_POINT}/security/oauth2/token", json=body, headers=headers)
+
+        data = response.json()
+        return data
+
+
+
